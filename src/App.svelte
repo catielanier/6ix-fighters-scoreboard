eb2d14e0c6405b910cce5c48988968b8dd2e02d6
Catie Lanier <hello@catielanier.ca>
2026-02-08 14:47:53 -0500

    add root route

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,27 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import { onMount } from "svelte";
+  import { Route, Router } from "svelte-routing";
+  import { getToken } from "./utils/tokenService";
+
+  import UpdateScoreboard from "./lib/UpdateScoreboard.svelte";
+  import Login from "./lib/Login.svelte";
+  let isLoggedIn: boolean | null;
+  let url: string = "";
+
+  onMount(() => {
+    isLoggedIn = !!getToken();
+  });
 </script>
 
-<main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+<Router {url}>
+  <main>
+    {#if isLoggedIn}
+      <Route path="/" component={UpdateScoreboard} />
+    {:else}
+      <Route path="/" component={Login} />
+    {/if}
+  </main>
+</Router>
 
 <style>
   .logo {
