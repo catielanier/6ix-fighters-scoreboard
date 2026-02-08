1ccebe4a189bc128f3c7db1caef9ae6d1a5a0c23
Catie Lanier <hello@catielanier.ca>
2026-02-08 14:33:03 -0500

    add token service

---
+++ src/utils/tokenService.ts
@@ -0,0 +1,4 @@
+export const getToken = (): string | null => localStorage.getItem("token");
+export const setToken = (token: string): void =>
+  localStorage.setItem("token", token);
+export const removeToken = (): void => localStorage.removeItem("token");
