653b547bce7f6e7e5746b87b12d27ed67b8c8c66
Catie Lanier <hello@catielanier.ca>
2026-02-08 15:21:16 -0500

    create types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -8,3 +8,53 @@ export interface FirebaseConfig {
   appId: string;
   measurementId: string;
 }
+
+export interface Player {
+  name: string;
+  imageUrl: string;
+}
+
+export interface Team {
+  name: string;
+  players: Player[];
+  score: number;
+  logoUrl: string;
+}
+
+export interface Division {
+  divisionNumber: number;
+  teams: Team[];
+}
+
+export interface Matchup {
+  teams: Team[];
+  homeScore: number;
+  awayScore: number;
+}
+
+export interface Leaderboard {
+  divisions: Division[];
+}
+
+export interface Timeout {
+  team: Team;
+}
+
+export interface MatchPlayer extends Player {
+  isWinner: boolean;
+}
+
+export interface IScoreboard {
+  homeTeam: {
+    activePlayer: MatchPlayer;
+    benchPlayer: MatchPlayer;
+    inactivePlayers: MatchPlayer[];
+    score: number;
+  };
+  awayTeam: {
+    activePlayer: MatchPlayer;
+    benchPlayer: MatchPlayer;
+    inactivePlayers: MatchPlayer[];
+    score: number;
+  };
+}
