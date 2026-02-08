3ead2c415c4ac124c099fa2a1130502786044e99
Catie Lanier <hello@catielanier.ca>
2026-02-08 15:00:47 -0500

    add login component

---
+++ src/lib/Login.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { setToken } from "../utils/tokenService";
+  import {
+    getAuth,
+    signInWithEmailAndPassword,
+    type UserCredential,
+  } from "firebase/auth";
+  import { firebase } from "../data/firebase";
+  export let setLogin: () => void;
+  let email: string = "";
+  let password: string = "";
+
+  const submitLogin = async (e: Event): Promise<void> => {
+    e.preventDefault();
+    const auth = getAuth(firebase);
+    signInWithEmailAndPassword(auth, email, password).then(
+      (user: UserCredential) => {
+        setToken(JSON.stringify(user));
+        setLogin();
+      },
+    );
+  };
+</script>
+
+<div class="login">
+  <form on:submit={submitLogin}>
+    <input type="email" bind:value={email} placeholder="Email address" />
+    <input type="password" bind:value={password} placeholder="Password" />
+    <button type="submit">Login</button>
+  </form>
+</div>
